8c935cdac611a7d708f380151bb1e398ab7d30bb
m.prashanth <prashanthmangena@gmail.com>
2024-05-28 14:52:06 -0700

    using time var instead of decrementing elements in piles list to find k in 875.py

--- 875. Koko Eating Bananas.py
+++ 875. Koko Eating Bananas.py
@@ -1,19 +1,19 @@
 class Solution:
-    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+    def minEatingSpeed(self, piles: list[int], h: int) -> int:
         # Brute force
         for k in range(1,max(piles)+1):
-            i, time = 0, 0
+            i, time = 0, h
             while (i <= len(piles) - 1):
                 if (piles[i] <= k):
                     i += 1
-                    time += 1
+                    time -= 1
                 else:
-                    piles[i] -= k
-                    time += 1
-                if (time > h):
+                    time -= (piles[i] // k)
+                    i += 1
+                if (time <= 0):
                     break
-            if (i == len(piles) - 1 and time <= h):
+            if (i == len(piles) - 1 and time >= 0):
                 return k
 
 obj = Solution()
-print(obj.sortArray([3,6,7,11],8))
+print(obj.minEatingSpeed([3,6,7,11],8))
