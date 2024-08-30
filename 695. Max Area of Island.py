89d86e78e300610ea4fc14d32bd3abdbf6f421e9
m.prashanth <prashanthmangena@gmail.com>
2024-08-30 09:59:41 -0700

    adds 695.MaxAreaOfIsland.py for debugging

---
+++ 695. Max Area of Island.py
@@ -0,0 +1,41 @@
+import collections
+
+class Solution:
+    def maxAreaOfIsland(self, grid: list[list[int]]) -> int:
+        if not grid:
+            return 0
+
+        rows, cols = len(grid), len(grid[0])
+        max_area = 0
+        visit = set()
+
+        def bfs(r, c):
+            q = collections.deque() 
+            visit.add((r, c)) 
+            q.append((r, c))
+            area = 0
+            while q:
+                row, col = q.pop()
+                directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+                for dr, dc in directions:
+                    r, c = row + dr, col + dc 
+                    if (r in range(rows) and c in range(cols) and grid[r][c] == 1 and (r, c) not in visit): 
+                        q.append((r, c)) 
+                        visit.add((r, c))
+                        area += 1
+            return area
+        
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1 and (r, c) not in visit:
+                    max_area = max(max_area, bfs(r, c))
+                    
+        return max_area
+
+test_obj = Solution()
+
+grid = [
+    [[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,1,0,0,0,0,0,0,0,0],[0,1,0,0,1,1,0,0,1,0,1,0,0],[0,1,0,0,1,1,0,0,1,1,1,0,0],[0,0,0,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]]
+  ]
+
+print(test_obj.numIslands(grid))
