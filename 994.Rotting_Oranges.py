05b6166794f03fdc3b3f5addbf6c61f141ae98b1
m.prashanth <prashanthmangena@gmail.com>
2024-09-05 10:36:03 -0700

    grid_2 Passes but grid_1 fails now in 994.py

--- 994.Rotting_Oranges.py
+++ 994.Rotting_Oranges.py
@@ -17,7 +17,7 @@ class Solution:
                         for y in range(COLS):
                             if (grid[x][y] == 1):
                                 return -1
-                            return length
+                    return length
                 
                 neighbors = [[0, 1], [0, -1], [1, 0], [-1, 0]]
                 for dr, dc in neighbors:
@@ -25,7 +25,7 @@ class Solution:
                         r + dr == ROWS or c + dc == COLS or
                         (r + dr, c + dc) in visit or grid[r + dr][c + dc] == 2):
                         continue
-                    if (grid[r][c] == 2):
+                    if (grid[r][c] == 2 and grid[r + dr][c + dc] != 0):
                         grid[r + dr][c + dc] = 2
                     queue.append((r + dr, c + dc))
                     visit.add((r + dr, c + dc))           
@@ -37,4 +37,5 @@ grid_1 = [[2,1,1],[1,1,0],[0,1,1]]
 grid_2 = [[2,1,1],[0,1,1],[1,0,1]]
 grid_3 = [[0,2]]
 
+print(test_obj.orangesRotting(grid_1))
 print(test_obj.orangesRotting(grid_2))
