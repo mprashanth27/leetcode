f2abee8cef011decb0b8996dc508ba823dcb424a
m.prashanth <prashanthmangena@gmail.com>
2024-10-06 13:42:03 -0700

    Fixes 198.py run time issues

--- 198.House_robber.py
+++ 198.House_robber.py
@@ -12,12 +12,12 @@ class Solution:
             if (max_val_index not in visited and max_val_index not in unvisitable):
                 res += max_val
 
+                if (max_val_index != len(nums_cpy) - 1): #check if we are popping the last element, as there will be no adj house on right
+                    unvisitable.add(max_val_index + 1)
+                    nums_cpy.pop(max_val_index + 1) 
+                
                 visited.add(max_val_index) 
                 nums_cpy.pop(max_val_index)
-                
-                if (max_val_index != len(nums_cpy) - 1): #check if we popped the last element, as there will be no adj house on right
-                    unvisitable.add(max_val_index + 1)
-                    nums_cpy.pop(max_val_index) # for popping (max_val_index + 1)'s val as it's index decrements by 1 after popping max_val
                 
                 if (max_val_index != 0): #check if we popped the first element, as there will be no adj house on left
                     unvisitable.add(max_val_index - 1)
@@ -28,4 +28,6 @@ class Solution:
 test_obj = Solution()
 
 nums = [1,2,3,1]
-test_obj.rob(nums)
+print(test_obj.rob(nums))
+
+'''Fixed the code but Worng approach - this solution tries to maximize the robbed amount by greedily selecting the maximum element'''
